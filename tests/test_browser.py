145ec990e3ff4218c5bd74219d4a2ef9a884001d
briehl <briehl@gmail.com>
2017-03-28 20:19:27 -0700

    went from cfg -> json, added browser tests

---
+++ tests/test_browser.py
@@ -0,0 +1,18 @@
+from .context import (
+    browse_for_what,
+    lexicon_file
+)
+import unittest
+
+class BrowserTestCase(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.lexicon = browse_for_what.Lexicon(lexicon_file)
+
+    def test_browser_setup(self):
+        browser = browse_for_what.Browser(self.lexicon, (2, 7), 5)
+        self.assertIsNotNone(browser)
+
+    def test_browsing(self):
+        browser = browse_for_what.Browser(self.lexicon, (2, 7), 5)
+        browser.browse()
