e7192402886dc305a28aaac916d463681af2a771
briehl <briehl@gmail.com>
2017-03-28 20:19:07 -0700

    good enough to start with

--- browse_for_what/__main__.py
+++ browse_for_what/__main__.py
@@ -1,3 +1,4 @@
 if __name__ == "__main__":
+    import os
     import browse_for_what.browseapp as app
-    app.start()
+    app.start(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "config.json")))
--- browse_for_what/browseapp.py
+++ browse_for_what/browseapp.py
@@ -1,5 +1,27 @@
-import requests
-from lexicon import Lexicon
+from .lexicon import Lexicon
+from .browser import Browser
+import json
+import time
+import os
 
-def start():
-    
+def start(config_file):
+    print("BROWSE DOWN FOR WHAT")
+    print(config_file)
+    config = json.loads(open(config_file, "r").read())
+    lex_file = config.get('lexicon_file', None)
+    if lex_file is None:
+        print('Your config.json file needs a valid "lexicon_file" key!')
+    l = Lexicon(_relative_path(lex_file))
+    b = Browser(l,
+                (config['min_words'], config['max_words']),
+                config['max_links_to_browse'])
+    sleep_time = config['wait_time']
+    b.browse()
+
+    for i in range(0, config['num_searches']):
+        time.sleep(sleep_time)
+        b.browse()
+
+def _relative_path(path):
+    root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+    return os.path.join(root_path, path)
